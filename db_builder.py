14ad5e1a180e7baf76b3133971ad82348228bec2
71619997a <gmarks@stuy.edu>
2016-10-16 17:04:37 -0400

    Separate test, add check for table existence

--- db_builder.py
+++ db_builder.py
@@ -8,18 +8,15 @@ db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
-#INSERT YOUR POPULATE CODE IN THIS ZONE
-#...perhaps by beginning with these examples...
+try:
+    q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
+    c.execute(q)    #run SQL query
 
+    q = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
+    c.execute(q)
+except sqlite3.OperationalError:
+    print 'Tables already created, skipping'
 
-q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
-
-c.execute(q)    #run SQL query
-
-
-q = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
-
-c.execute(q)
 with open('peeps.csv') as f:
     student_reader = csv.DictReader(f)
     for row in student_reader:
@@ -38,11 +35,15 @@ with open('courses.csv') as f:
         q = 'INSERT INTO students VALUES('+code+','+mark+','+id+')'
         c.execute(q)
 
-for i in c.execute('SELECT * FROM students'):
-    print i
-print
-for i in c.execute('SELECT * FROM courses'):
-    print i
+def testdb():  # tester
+    for i in c.execute('SELECT * FROM students'):
+        print i
+    print '\n'
+    for i in c.execute('SELECT * FROM courses'):
+        print i
+
+# testdb()
+
 #==========================================================
 db.commit() #save changes
 db.close()  #close database
